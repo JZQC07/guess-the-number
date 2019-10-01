1b7486e78fc5e27d83ee354cc3cfd504ee7d4564
Jonatan Schultz <jonatan_schultz@hotmail.com>
2019-10-01 15:58:50 +0200

    Needs exception handling

--- Program.cs
+++ Program.cs
@@ -2,11 +2,45 @@
 
 namespace guess_the_number
 {
-    class Program
+    public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            while (true)
+            {
+                Random randomerare = new Random();
+                int nummer = randomerare.Next(1, 100);
+                int antalgissningar = 1;
+                int gissning = 101;
+                Console.WriteLine("Välkommen! Ett tal mellan 1-100 har slumpats ut.");
+                Console.WriteLine("Du har 10 chanser på dig att gissa rätt tal!");
+                while (nummer != gissning && antalgissningar < 11)
+                {
+                    Console.WriteLine("Gissning nummer {0}: ", antalgissningar);
+                    gissning = int.Parse(Console.ReadLine());
+                    antalgissningar++;
+                }
+                if (nummer == gissning)
+                {
+                    Console.WriteLine("Du lyckades gissa rätt med {0} försök kvar!", antalgissningar);
+                    Console.WriteLine("Grattis!");
+                }
+                else if (antalgissningar > 10)
+                {
+                    Console.WriteLine("Du har gissat för många gånger...");
+                    Console.WriteLine("Du förlorar.");
+                }
+                Console.WriteLine("Spela igen?? [J] / [N]");
+                string playAgain = Console.ReadLine();
+                if(playAgain == "N")
+                {   
+                    Console.WriteLine("Tryck valfri tangent för att avsluta programmet..");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+            }
+
         }
     }
 }
